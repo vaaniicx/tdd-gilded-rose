1d34e3f5ee23c0d3981948e70370b41d05cfd055
vaaniicx <vanessakausl@gmail.com>
2024-05-01 02:47:13 +0200

    Add test cases

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,107 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
+    private GildedRose underTest;
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    @DisplayName("Aged Brie increases in quality")
+    void test_agedBrieQualityIncrease() {
+        Item[] items = { new Item("Aged Brie", 200, 0) };
+        underTest = new GildedRose(items);
+
+        underTest.updateQuality();
+
+        assertThat(items[0].quality)
+                .isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("The quality of an item cannot be higher than 50")
+    void test_maximumItemQuality() {
+        Item[] items = { new Item("Aged Brie", 200, 50) };
+        underTest = new GildedRose(items);
+
+        underTest.updateQuality();
+
+        assertThat(items[0].quality)
+                .isEqualTo(50);
+    }
+
+    @Test
+    @DisplayName("Sulfuras never has to be sold")
+    void test_sulfurasSellIn() {
+        Item[] items = { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        underTest = new GildedRose(items);
+
+        underTest.updateQuality();
+
+        assertThat(items[0].sellIn)
+                .isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Sulfuras never decreases in quality")
+    void test_sulfurasQualityDecrease() {
+        Item[] items = { new Item("Sulfuras, Hand of Ragnaros", 200, 80) };
+        underTest = new GildedRose(items);
+
+        underTest.updateQuality();
+
+        assertThat(items[0].quality)
+                .isEqualTo(80);
+    }
+
+    @Test
+    @DisplayName("Quality of Backstage Passes increases by 2 when there are 10 days or less")
+    void test_backstagePassQualityIncreaseByTwo() {
+        Item[] items = { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) };
+        underTest = new GildedRose(items);
+
+        underTest.updateQuality();
+
+        assertThat(items[0].quality)
+                .isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("Quality of Backstage Passes increases by 3 when there are 5 days or less")
+    void test_backstagePassQualityIncreaseByThree() {
+        Item[] items = { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) };
+        underTest = new GildedRose(items);
+
+        underTest.updateQuality();
+
+        assertThat(items[0].quality)
+                .isEqualTo(13);
+    }
+
+    @Test
+    @DisplayName("Quality of Backstage Passes drops to 0 after the concert")
+    void test_backstagePassQualityDropAfterConcert() {
+        Item[] items = { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) };
+        underTest = new GildedRose(items);
+
+        underTest.updateQuality();
+
+        assertThat(items[0].quality)
+                .isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Once the sell by date has passed, Quality degrades twice as fast")
+    void test_decreaseInQualityTwiceAsFast() {
+        Item[] items = { new Item("+5 Dexterity Vest", -1, 10) };
+        underTest = new GildedRose(items);
+
+        underTest.updateQuality();
+
+        assertThat(items[0].quality)
+                .isEqualTo(8);
+    }
 }
