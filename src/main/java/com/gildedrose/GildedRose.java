5675527adebe08510aaf166ab03f4bab252acb52
vaaniicx <vanessakausl@gmail.com>
2024-05-01 12:24:35 +0200

    Replace constructor with lombok annotation

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,12 +2,11 @@ package com.gildedrose;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 class GildedRose {
     private final List<Item> items;
-
-    public GildedRose(List<Item> items) {
-        this.items = items;
-    }
 
     public void updateQuality() {
         items.forEach(Item::updateItem);
