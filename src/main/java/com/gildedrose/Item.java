a045f669e31e28f9df64741d3c3a5e28d8f53af1
vaaniicx <vanessakausl@gmail.com>
2024-05-01 10:04:12 +0200

    Add lombok annotations to Item.java

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,5 +1,10 @@
 package com.gildedrose;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
 public class Item {
 
     public String name;
@@ -7,15 +12,4 @@ public class Item {
     public int sellIn;
 
     public int quality;
-
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
-
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
 }
