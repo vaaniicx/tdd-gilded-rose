c5d9825b677c4106158af1b5d99b281a05727a36
vaaniicx <vanessakausl@gmail.com>
2024-05-01 11:59:33 +0200

    Add ItemType.DEXTERITY_VEST

--- src/main/java/com/gildedrose/ItemType.java
+++ src/main/java/com/gildedrose/ItemType.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum ItemType {
     AGED_BRIE("Aged Brie"),
     BACKSTAGE_PASS("Backstage passes to a TAFKAL80ETC concert"),
+    DEXTERITY_VEST("+5 Dexterity Vest"),
     SULFURAS("Sulfuras, Hand of Ragnaros");
 
     private final String name;
