17ec07e0844cfb3a19619f90ca2b0d4278409112
vaaniicx <vanessakausl@gmail.com>
2024-05-01 12:00:08 +0200

    Replace strings by their enum values in test cases

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,7 +12,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("Aged Brie increases in quality")
     void test_agedBrieQualityIncrease() {
-        Item[] items = { new Item("Aged Brie", 1, 0) };
+        Item[] items = { new Item(ItemType.AGED_BRIE.getName(), 1, 0) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
@@ -24,7 +24,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("Aged Brie increases twice in quality")
     void test_agedBrieQualityIncreaseTwice() {
-        Item[] items = { new Item("Aged Brie", 0, 0) };
+        Item[] items = { new Item(ItemType.AGED_BRIE.getName(), 0, 0) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
@@ -36,7 +36,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("The quality of an item cannot be higher than 50")
     void test_maximumItemQuality() {
-        Item[] items = { new Item("Aged Brie", 200, 50) };
+        Item[] items = { new Item(ItemType.AGED_BRIE.getName(), 200, 50) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
@@ -48,7 +48,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("Sulfuras never has to be sold")
     void test_sulfurasSellIn() {
-        Item[] items = { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        Item[] items = { new Item(ItemType.SULFURAS.getName(), 0, 80) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
@@ -60,7 +60,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("Sulfuras never decreases in quality")
     void test_sulfurasQualityDecrease() {
-        Item[] items = { new Item("Sulfuras, Hand of Ragnaros", 200, 80) };
+        Item[] items = { new Item(ItemType.SULFURAS.getName(), 200, 80) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
@@ -72,7 +72,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("Quality of Backstage Passes increases by 2 when there are 10 days or less")
     void test_backstagePassQualityIncreaseByTwo() {
-        Item[] items = { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) };
+        Item[] items = { new Item(ItemType.BACKSTAGE_PASS.getName(), 10, 10) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
@@ -84,7 +84,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("Quality of Backstage Passes increases by 3 when there are 5 days or less")
     void test_backstagePassQualityIncreaseByThree() {
-        Item[] items = { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) };
+        Item[] items = { new Item(ItemType.BACKSTAGE_PASS.getName(), 5, 10) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
@@ -96,7 +96,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("Quality of Backstage Passes drops to 0 after the concert")
     void test_backstagePassQualityDropAfterConcert() {
-        Item[] items = { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) };
+        Item[] items = { new Item(ItemType.BACKSTAGE_PASS.getName(), 0, 10) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
@@ -108,7 +108,7 @@ class GildedRoseTest {
     @Test
     @DisplayName("Once the sell by date has passed, Quality degrades twice as fast")
     void test_decreaseInQualityTwiceAsFast() {
-        Item[] items = { new Item("+5 Dexterity Vest", -1, 10) };
+        Item[] items = { new Item(ItemType.DEXTERITY_VEST.getName(), -1, 10) };
         underTest = new GildedRose(items);
 
         underTest.updateQuality();
